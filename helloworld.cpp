26746b1b4f4972182f23ca33b1008392ce39f5e4
SATHYADEVIK <34641557+SATHYADEVIK@users.noreply.github.com>
2018-03-18 21:59:38 +0530

    Create helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+
+/*my first 
+program in c++ block comment*/
+
+#include<iostream>//preprocessor
+using namespace std;//standard std library under which cout cin are all present
+int main()//function which is called
+{
+	cout<<"Hello Sathya";//line comment with << insertion operator
+	cout<<"Hai";//Hai is inserted ito screen(cout) using insertion operator
+	return 0;
+}
